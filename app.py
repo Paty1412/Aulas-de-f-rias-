3fc1ee9140a1d072428d6d3514eaa157fda8b64d
Paty1412 <154523505+Paty1412@users.noreply.github.com>
2024-07-23 21:57:33 -0300

    Exercicio1

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("teste")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Olá"
+
+@app.route("/teste")
+def teste ():    
+    return "Pagina na web"
+
+if __name__ =="__main__":
+    app.run()
